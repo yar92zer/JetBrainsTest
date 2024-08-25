c62c69177cd51d15a3e5520a56dc9c16e86d00f7
yar92zer <yar199199@gmail.com>
2024-08-25 20:36:11 +0400

    Decompiler v1.1

--- src/test/java/com/example/jetbrainstest/pages/DecompilerPage.java
+++ src/test/java/com/example/jetbrainstest/pages/DecompilerPage.java
@@ -8,15 +8,22 @@ import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://www.jetbrains.com/decompiler/
 public class DecompilerPage {
-WebDriver driver;
-@FindBy(css ="[data-test='button']")
+    WebDriver driver;
+    @FindBy(css = "[data-test='button']")
     private WebElement downloadButton;
-public Boolean checkIfDownloadButtonIsClickable() {
-    System.out.println("Проверка активности кнопки загрузки");
-    return downloadButton.isEnabled();
-}
-public DecompilerPage(WebDriver driver) {
-    this.driver = driver;
-    PageFactory.initElements(driver, this);
-}
+
+    public boolean checkIfDownloadButtonIsClickable() {
+        System.out.println("Проверка активности кнопки Download");
+        return downloadButton.isEnabled();
+    }
+
+    public boolean GetDotPeekIsActive() {
+        System.out.println("Проверка доступности кнопки 'Get dotPeek'");
+        return downloadButton.isEnabled();
+    }
+
+    public DecompilerPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/test/java/com/example/jetbrainstest/tests/DecompilerTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DecompilerTest.java
@@ -1,15 +1,15 @@
 package com.example.jetbrainstest.tests;
 
 import com.example.jetbrainstest.pages.DecompilerPage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class DecompilerTest extends BaseTest {
 
     private DecompilerPage decompilerPage;
+
     @BeforeEach
     @Override
     public void setUp() {
@@ -19,8 +19,15 @@ public class DecompilerTest extends BaseTest {
     }
 
     @Test
+    @DisplayName("Проверка активности кнопки Get dotPeek")
+    public void getDotPeekButtonIsActive() {
+        decompilerPage = new DecompilerPage(getDriver());
+        Assertions.assertTrue(decompilerPage.GetDotPeekIsActive(), "Get dotPeek кнопка не активна");
+    }
+
+    @Test
     @DisplayName("Проверкаб что конопка скчивание активвна")
-            public void buttonCheck(){
-        assertTrue(decompilerPage.checkIfDownloadButtonIsClickable(),"Кнопка скачивание не активна");
+    public void buttonCheck() {
+        Assertions.assertTrue(decompilerPage.checkIfDownloadButtonIsClickable(), "Кнопка скачивание не активна");
     }
 }
