eae14a25730ec1440830c053b6d36794749e5433
yar92zer <yar199199@gmail.com>
2024-09-02 19:04:50 +0400

    Decompiler v1.5 Step

--- src/test/java/com/example/jetbrainstest/tests/DecompilerTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DecompilerTest.java
@@ -1,6 +1,7 @@
 package com.example.jetbrainstest.tests;
 
 import com.example.jetbrainstest.pages.DecompilerPage;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ public class DecompilerTest extends BaseTest {
 
     @BeforeEach
     @Override
+    @Step("Переход в раздел dotPeek")
     public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/decompiler/");
@@ -20,6 +22,7 @@ public class DecompilerTest extends BaseTest {
 
     @Test
     @DisplayName("Проверка активности кнопки Get dotPeek")
+    @Step("Get dotPeek кнопка не активна")
     public void DotPeekIsActive() {
         decompilerPage = new DecompilerPage(getDriver());
         Assertions.assertTrue(decompilerPage.GetDotPeekIsActive(), "Get dotPeek кнопка не активна");
